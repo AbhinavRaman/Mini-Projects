005fa97b5d4fe993ce64ee4c3aad5b583bbda510
AbhinavRaman <abhinavraman102@gmail.com>
2025-05-16 16:46:57 +0530

    made gallery section, newletter section and footer section

--- Tailwind-Project/src/script.js
+++ Tailwind-Project/src/script.js
@@ -1,4 +1,29 @@
-    function toggleMenu() {
-      const menu = document.getElementById('mobile-menu');
-      menu.classList.toggle('hidden');
-    }
+//toggle for responsive navbar
+function toggleMenu() {
+  const menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('hidden');
+}
+
+//slide for expert chefs part
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slider = document.getElementById("slider");
+  const slides = document.querySelectorAll("#slider > div");
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) currentIndex = 0;
+  if (index < 0) currentIndex = totalSlides - 1;
+
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex++;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  showSlide(currentIndex);
+}
